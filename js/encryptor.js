2e746368f9666ce85b31e6a397e63be57c7d5cf5
Tim Heuett <tim.heuett@gmail.com>
2015-04-07 22:16:29 -0700

    adding validators for form

--- js/encryptor.js
+++ js/encryptor.js
@@ -21,7 +21,20 @@
 
 },{"../vendor/aes.js":3}],2:[function(require,module,exports){
 (function() {
+  validateEncryptForm(function() {
+    if ($('.message-area').val() === "") {
+      error('.message-area', 'You must enter a message!');
+      return false;
+    }
+    if ($('.secret-key-input').val() === "") {
+      error('.secret-key-input', 'You must enter a secret key!');
+      return false;
+    }
+  });
 
+  error(function(field, message) {
+    return $(field).wrap($("div").addClass("error").prepend("<p>" + message + "</p>"));
+  });
 
 }).call(this);
 
