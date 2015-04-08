1fab18625f9aa76fa5e13954c9ce3a9a3a7bc435
Tim Heuett <tim.heuett@gmail.com>
2015-04-07 23:12:15 -0700

    add second pane and generate url

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,17 @@
-var error, validateEncryptForm;
+var encryptMessage, error, submitForm, validateEncryptForm;
+
+$(function() {
+  return $('#encrypt-button').click(submitForm);
+});
+
+submitForm = function() {
+  if (!validateEncryptForm()) {
+    return;
+  }
+  $('.share-link').val(encryptMessage);
+  $("#pane1").hide();
+  return $("#pane2").show();
+};
 
 validateEncryptForm = function() {
   $('.error p').remove();
@@ -11,11 +24,19 @@ validateEncryptForm = function() {
     error('.secret-key-input', 'You must enter a secret key!');
     return false;
   }
+  return true;
 };
 
 error = function(field, message) {
   $(field).wrap($('<div>').addClass('error'));
   return $(field).parent().prepend($('<p>').append(message));
+};
+
+encryptMessage = function() {
+  var key, message;
+  message = $('.message-area').val();
+  key = $('.secret-key-input').val();
+  return window.location.origin + "/decode.html?key=" + encryptor.encrypt(message, key);
 };
 
 //# sourceMappingURL=ui.js.map
--- src/js/ui.coffee
+++ src/js/ui.coffee
@@ -1,3 +1,13 @@
+$ ->
+  $('#encrypt-button').click(submitForm)
+
+submitForm = ->
+  if !validateEncryptForm()
+    return
+  $('.share-link').val(encryptMessage)
+  $("#pane1").hide()
+  $("#pane2").show()
+
 validateEncryptForm = ->
   $('.error p').remove()
   $('.error').children().unwrap()
@@ -7,7 +17,14 @@ validateEncryptForm = ->
   if $('.secret-key-input').val() == ''
     error '.secret-key-input', 'You must enter a secret key!'
     return false
+  true
 
 error = (field, message) ->
   $(field).wrap( $('<div>').addClass('error') )
   $(field).parent().prepend( $('<p>').append(message) )
+
+encryptMessage = ->
+  message = $('.message-area').val()
+  key = $('.secret-key-input').val()
+
+  window.location.origin + "/decode.html?key=" + encryptor.encrypt(message, key)
